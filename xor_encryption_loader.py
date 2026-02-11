256ce65dfa6ec9a16f8a34a9afd35608fd26cbc8
Abdulgani_dev <112663385+ABDUJDHF@users.noreply.github.com>
2026-02-11 03:16:46 +0300

    Adding XOR encryption loader for Chestfarming-op

---
+++ xor_encryption_loader.py
@@ -0,0 +1,15 @@
+# XOR Encryption Loader
+
+import sys
+
+key = b'secret_key'
+
+def xor_encrypt(data, key):
+    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+
+if __name__ == '__main__':
+    with open('Chestfarming-op', 'rb') as f:
+        data = f.read()
+    encrypted_data = xor_encrypt(data, key)
+    with open('Chestfarming-op.enc', 'wb') as f:
+        f.write(encrypted_data)
